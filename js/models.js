f01684ec3a5bed6ec5270c5b471c6c2be4419596
vadimmarkelov <markelov.vadim@gmail.com>
2013-11-28 00:25:34 +0200

    Update models.js

--- js/models.js
+++ js/models.js
@@ -140,16 +140,19 @@ function CitiesList (settings){
 		_data=[].slice.call(settings.data); //make local copy of data
 		_init();
 	} else {//load data from URL
-		$.ajax({
-                  cache: false,
-                  dataType: "json",
-                  url: settings.dataUrl,
-                  method: "GET"
-                 })
-                 .done(function(response){
-		  _data=response;
-                  _init();	
-		 });
-	}
+           $.ajax({
+             cache: false,
+             dataType: "json",
+             url: settings.dataUrl,
+             method: "GET",
+             success: function(response){
+                 _data=response;
+                 _init();
+             },
+             error: function(jqXHR, exception, errorThrown){
+                 //throwError('Data load has been failed!</h2> <h2>Error details: ('+jqXHR.status+') '+errorThrown)
+             }
+           });
+     }
 
 }
